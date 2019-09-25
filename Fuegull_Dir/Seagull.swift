7feae0e1d5b9e790781e446a942e5fab8dbefec7
Adam Cabral <adam_cabral@my.uri.edu>
2019-09-24 22:52:48 -0400

    Bird class modifications

--- Fuegull_Dir/Seagull.swift
+++ Fuegull_Dir/Seagull.swift
@@ -18,6 +18,14 @@ class seagull{
         bird = SKSpriteNode()
         bird.size.width = 70
         bird.size.height = 70
+        bird.position = CGPoint(x: -566, y: 1)
+        bird.physicsBody?.affectedByGravity = true
+        bird.physicsBody?.pinned = false
+        bird.physicsBody = SKPhysicsBody(texture: SKTexture(image: #imageLiteral(resourceName: "triangle")), size: CGSize(width: 70, height: 70))
+        bird.physicsBody?.isDynamic = true
+        bird.physicsBody?.categoryBitMask = 8 //From Enum in GameScene.sks
+        bird.physicsBody?.collisionBitMask = 3
+        
     }
 }
 
